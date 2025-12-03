3ddecf23a1982fe6026aeb0af1df7fe06230f7e1
oyewolz <oyewoletaiwo219@gmail.com>
2025-12-03 11:07:22 -0700

    feat(password hash)- implement password hashing

--- services/user-service/src/user_service/services/user_service.py
+++ services/user-service/src/user_service/services/user_service.py
@@ -4,20 +4,26 @@ from shared_lib.database.models.user.ft_user import FTUser
 from shared_lib.dto.user import UserDto, UserLoginDto
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from passlib.context import CryptContext
+from passlib.hash import bcrypt
 
 class UserService: 
-    
+
+    @staticmethod
+    def hash_password(password: str): 
+        return bcrypt.hash(password)
 
     async def register_user(self, user: UserDto, session: AsyncSession): 
           
           if user.password != user.confirm_password: 
-            raise ValueError("Passwords do not match")
+            raise HTTPException(status_code=400, detail="Passwords do not match")
           
           user_exist = await session.execute(select(FTUser).filter_by(email=user.email))
           if user_exist.scalars().first(): 
             raise HTTPException(status_code=400, detail="User already exists")
 
           ft_user = FTUser.model_validate(user)
+          ft_user.password = self.hash_password(user.password)
           session.add(ft_user)
           await session.commit()
           await session.refresh(ft_user)
@@ -25,15 +31,17 @@ class UserService:
           del ft_user.password
           return ft_user
     
-    async def login_user(self, user: UserLoginDto, session: AsyncSession):
+    async def login_user(self, userDto: UserLoginDto, session: AsyncSession):
         # find user by email
-        result = await session.execute(select(FTUser).filter_by(email=user.email))
-        userDto = result.scalars().first()
-        if not userDto:
+        result = await session.execute(select(FTUser).filter_by(email=userDto.email))
+        user = result.scalars().first()
+        if not user:
             raise HTTPException(status_code=400, detail="User not found")
-        if userDto.password != user.password:
+        if self.hash_password(userDto.password) != user.password:
             raise HTTPException(status_code=400, detail="Invalid password")
-        return userDto
+        
+        del user.password
+        return user
  
 
     async def get_user(self, user_id: int, session: AsyncSession):
